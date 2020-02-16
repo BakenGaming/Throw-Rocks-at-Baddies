7a5edb83d002bbf627080e8b92c89d4c1207c8ad
BakenGaming <48033196+BakenGaming@users.noreply.github.com>
2020-02-16 13:48:53 -0500

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private AudioManager audioManager;
+    void Start()
+    {
+        audioManager = AudioManager.instance;
+        if (audioManager == null)
+        {
+            Debug.Log("NO AUDIO MANAGER IN SCENE");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] private Rigidbody2D playerRigidbody2D;
+
+    public int speed;
+
+    public void MovePlayer(float _directionX, float _directionY)
+    {
+        playerRigidbody2D.velocity = new Vector2(_directionX, _directionY) * speed;
+    }
+}
