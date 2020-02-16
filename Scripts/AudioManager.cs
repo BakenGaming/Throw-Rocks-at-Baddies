da644722dab0452a66250918e3f7ec6334634db4
BakenGaming <48033196+BakenGaming@users.noreply.github.com>
2020-02-16 13:58:13 -0500

    Add files via upload

---
+++ Scripts/AudioManager.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+    public AudioClip audioClip;
+    private AudioSource audioSource;
+
+    [Range(0f, 1f)]
+    public float volume;
+    [Range(0.5f, 1.5f)]
+    public float pitch;
+    public void SetSource(AudioSource _source)
+    {
+        audioSource = _source;
+        audioSource.clip = audioClip;
+       
+    }
+
+    public void Play()
+    {
+        audioSource.volume = volume;
+        audioSource.pitch = pitch;
+        audioSource.Play();
+
+    }
+}
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+
+    [SerializeField] private Sound[] sounds;
+
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("No Audio Manager In Scene");
+        }
+        else 
+        {
+            instance = this;
+        }
+
+    }
+    private void Start()
+    {
+        for (int i = 0; i < sounds.Length; i++)
+        {
+            GameObject _go = new GameObject("Sound_" + i + "_" + sounds[i].name);
+            _go.transform.SetParent(this.transform);
+            sounds[i].SetSource(_go.AddComponent<AudioSource>());
+        }
+
+    }
+
+    public void PlaySound(string _name)
+    {
+        for (int i = 0; i < sounds.Length; i++)
+        {
+            if (sounds[i].name == _name)
+            {
+                sounds[i].Play();
+                return;
+            }
+        }
+        Debug.LogWarning("Audiomanager : Sound not found");
+    }
+
+}
---
+++ Scripts/PlayerInput.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerInput : MonoBehaviour
+{
+
+    [SerializeField] private PlayerController playerScript;
+    [SerializeField] private Shovel shovelScript;
+    [SerializeField] private LayerMask whatIsHole;
+
+
+    private float digCoolDownTimer = 0f;
+    public float digCoolDown;
+
+    public float digRadius;
+    private bool alreadyDug;
+    private float directionX, directionY;
+
+
+    private Animator anim;
+
+    [Header("Events")]
+    [Space]
+
+    public UnityEvent wasDugEvent;
+    public UnityEvent diggingEvent;
+
+    [System.Serializable]
+    public class BoolEvent : UnityEvent<bool> { }
+
+    private void Awake()
+    {
+        if (wasDugEvent == null)
+            wasDugEvent = new UnityEvent();
+
+        if (diggingEvent == null)
+            diggingEvent = new UnityEvent();
+    }
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        
+    }
+    void Update()
+    {
+        directionX = Input.GetAxisRaw("Horizontal");
+        directionY = Input.GetAxisRaw("Vertical");
+
+    if (digCoolDownTimer <= 0)
+    {
+            bool wasDug = alreadyDug;
+        alreadyDug = false;
+
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(shovelScript.digPosition.position, digRadius, whatIsHole);
+
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+            {
+                alreadyDug = true;
+                if (wasDug)
+                    wasDugEvent.Invoke();
+            }
+        }
+        if (Input.GetMouseButtonDown(0) && !wasDug)
+        {
+            anim.SetBool("isDigging", true);
+            shovelScript.DigHole();
+            digCoolDownTimer = digCoolDown;
+        }
+        else
+        {
+            anim.SetBool("isDigging", false);
+        }
+     }
+     else
+     {
+            digCoolDownTimer -= Time.deltaTime;
+     }
+
+    }
+
+    private void FixedUpdate()
+    {
+        playerScript.MovePlayer(directionX, directionY);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(shovelScript.digPosition.transform.position, digRadius);
+    }
+}
